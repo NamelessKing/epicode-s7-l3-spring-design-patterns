e650e72dd6ef94febd098d0768888e464edc01d7
Tanjin <NamelessKing@users.noreply.github.com>
2026-02-18 17:05:55 +0100

    frst commit

---
+++ src/main/java/it/epicode/spring/s7l3/designpatterns/EpicodeS7L3SpringDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.spring.s7l3.designpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicodeS7L3SpringDesignPatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpicodeS7L3SpringDesignPatternsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/epicode/spring/s7l3/designpatterns/EpicodeS7L3SpringDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.spring.s7l3.designpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicodeS7L3SpringDesignPatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
